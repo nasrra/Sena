9691bae94de5b0f4bc35c467174763db7e242c02
naserra <naserra.work@gmail.com>
2025-08-08 18:02:57 +1000

    properly hooked up player ability tutorial

--- Source/interactables/PlayerActionAcquirer.cs
+++ Source/interactables/PlayerActionAcquirer.cs
@@ -45,14 +45,27 @@ public partial class PlayerActionAcquirer : Node2D{
         if(player == null || player.IsPlayerActionEnabled(playerActions) == true){
             return;
         }
-        GameplayGui.Singleton.TutorialGui.EnableAttackTutorial();
         player.EnablePlayerAction(playerActions);
+        EnableTutorial();
         interactable.DisableInteraction();
     }
 
     private void OnInteractorPriorityStateCallback(Interactor interactor){
         if(interactor.GetParent() is Player player && player.IsPlayerActionEnabled(playerActions)==true){
             interactable.DisableInteraction();
+        }
+    }
+
+    private void EnableTutorial(){
+        switch(playerActions){
+            case PlayerActions.Attack:
+                GameplayGui.Singleton.TutorialGui.EnableAttackTutorial();
+            break;
+            case PlayerActions.Heal:
+                GameplayGui.Singleton.TutorialGui.EnableHealTutorial();
+            break;
+            default:
+                throw new NotImplementedException($"{playerActions} not implemented!");
         }
     }
 
